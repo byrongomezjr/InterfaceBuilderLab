7f08a4a08b43d1ea7194b1ffbbae9bdcc4803d94
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-02-16 12:00:23 -0500

    lab.

--- InterfaceBuilder_Gomez/ViewController.swift
+++ InterfaceBuilder_Gomez/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var textToggle = true
+    @IBOutlet var mainLaber: UILabel!
+    
+    @IBAction func changeTitle(_ sender: UIButton
+    ) {
+        textToggle.toggle();
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
